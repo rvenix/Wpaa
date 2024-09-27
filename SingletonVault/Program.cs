3678a2a5e2c5716b43f222656c058a8b27256af8
mroguski@wp.pl <mroguski@wp.pl>
2024-09-27 20:11:57 +0200

    Add project files.

---
+++ SingletonVault/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonVault;
+
+Console.WriteLine("Hello, World!");
+
+Vault accesKey = Vault.Instance;
+
+
+Console.WriteLine(accesKey.GetAccessKey());
+
+accesKey = Vault.Instance;
+
+Console.WriteLine(accesKey.GetAccessKey());
+
+Console.ReadKey();
---
+++ SingletonVault/Vault.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonVault
+{
+    public sealed class Vault
+    {
+        private static Vault AccessKey;
+        private static object syncRoot = new object();
+        private static string key;
+
+        private Vault() 
+        {
+            Random rnd = new Random();
+            key = rnd.Next(0, 100).ToString() + rnd.Next(0, 1000);
+        }
+
+        public static Vault Instance
+        {
+            get
+            {
+                lock (syncRoot)
+                { 
+                    if(AccessKey == null)
+                    {
+                        AccessKey = new Vault();
+                    }
+                }
+
+                return AccessKey;
+            }
+        }
+
+        public string GetAccessKey()
+        {
+            return key;
+        }
+    }
+}
