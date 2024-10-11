3a9a5d7bb3193a9b4eb4e566a1db84275e35685c
mroguski@wp.pl <mroguski@wp.pl>
2024-10-11 20:46:01 +0200

    Prototype Add

---
+++ Prototype/Ork.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Ork
+    {
+        public string Nazwa { get; set; }
+
+        public int Sila { get; set; }
+
+        public Ork(string nazwa, int sila)
+        {
+            Nazwa = nazwa;
+            Sila = sila;
+        }
+
+        public Ork Clone()
+        {
+            string json = JsonConvert.SerializeObject(this);
+
+            // Deserializacja z JSON (nowy obiekt)
+            return JsonConvert.DeserializeObject<Ork>(json);
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Prototype;
+
+Ork ork = new Ork("Stefan", 100);
+
+List<Ork> orks = new List<Ork>();
+
+Random random = new Random();
+
+for (int i = 0; i < 100; i++)
+{ 
+    Ork klonOrka = ork.Clone();
+
+    klonOrka.Sila = random.Next(1, 1000);
+    klonOrka.Nazwa = "Stefan" + (i + 1).ToString();
+
+    orks.Add(klonOrka);
+}
+
+
+foreach (var orkItem in orks)
+{
+    Console.WriteLine($"Nazwa: {orkItem.Nazwa}, Siła: {orkItem.Sila}");
+}
+
+Console.ReadKey();
