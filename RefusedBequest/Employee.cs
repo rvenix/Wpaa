654e3dae11049194560a62c2f6d8939fe21ac7fc
mroguski@wp.pl <mroguski@wp.pl>
2024-10-11 19:48:07 +0200

    Refused Bequest add

---
+++ RefusedBequest/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefusedBequest
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public void Work() { /* Implementacja */ }
+        public void AttendMeeting() { /* Implementacja */ }
+    }
+}
---
+++ RefusedBequest/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefusedBequest
+{
+    public class Manager : IEmployee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public void ManageTeam() { /* Implementacja */ }
+
+
+        public void AttendMeeting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
