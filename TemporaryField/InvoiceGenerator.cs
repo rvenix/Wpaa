b33ffdd599fc37298f0a01c22341639c9affef3e
mroguski@wp.pl <mroguski@wp.pl>
2024-10-11 20:04:01 +0200

    Temporary Field add

---
+++ TemporaryField/InvoiceGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemporaryField
+{
+    public class InvoiceGenerator
+    {
+        public void GenerateInvoice()
+        {
+            int invoiceNumber = 12345;
+            PdfWriter pdfWriter = new PdfWriter($"Invoice_{invoiceNumber}.pdf");
+
+            // Generowanie faktury
+            pdfWriter.Write("Invoice Content");
+            pdfWriter.Close();
+        }
+
+        public void OtherMethod()
+        {
+            // Metoda, która nie korzysta z tymczasowych pól
+        }
+    }
+
+    public class PdfWriter
+    {
+        public PdfWriter()
+        {
+                
+        }
+
+        public PdfWriter(string pdf)
+        {
+            
+        }
+
+        public void Write(string text) { }
+        public void Close() { }
+    }
+}
