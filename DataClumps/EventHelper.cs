9ed3c92df9b517918844dec7d165393e2e733e15
mroguski@wp.pl <mroguski@wp.pl>
2024-10-11 20:07:11 +0200

    Data Clumps add

---
+++ DataClumps/EventHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataClumps
+{
+    public static class EventHelper
+    {
+        public static void RegisterEvent(EventDetails eventDetails)
+        {
+            Console.WriteLine($"Event: {eventDetails.EventName}, Date: {eventDetails.EventDate}, Location: {eventDetails.Location}");
+        }
+    }
+}
