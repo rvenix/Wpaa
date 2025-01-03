923ec8dfa2f2f93df2aefaf68b08265cb1dfd710
mroguski@wp.pl <mroguski@wp.pl>
2025-01-03 21:09:03 +0100

    GildedRose fixes

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -19,24 +19,32 @@ public class GildedRose
             {
                 Items[i].Quality = Items[i].Quality - 1;
             }
-            else if (Items[i].Quality < 50)
+            else
             {
-                Items[i].Quality = Items[i].Quality + 1;
-
-                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Quality < 50)
                 {
-                    if (Items[i].SellIn < 11)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+                    Items[i].Quality = Items[i].Quality + 1;
 
-                    if (Items[i].SellIn < 6)
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        if (Items[i].SellIn < 11)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+
+                        if (Items[i].SellIn < 6)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
                     }
                 }
             }
-
 
             if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
             {
